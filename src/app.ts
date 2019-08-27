d7675a3b8730d0aa646dc109f7e50ff009568564
Christopher Willard <thatguychrisw@gmail.com>
2019-08-26 21:32:31 -0400

    Log request body 🔎

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,19 @@
 import express from 'express';
+import bodyParser from 'body-parser';
 import serverless from 'serverless-http';
 import WeatherApiService from "./weather-api-service";
 
 const app = express();
+app.use(bodyParser.json());
+
 const weatherService = new WeatherApiService({});
 
 app.get('/', (request, response) => response.send('💙 Netlify Functions!'));
 
 app.post('/', (request, response) => {
     let temperature = weatherService.temperature(33458).toString();
+
+    console.log(request.body);
 
     response.send(`The current temperature placeholder is: ${temperature}.`);
 });
